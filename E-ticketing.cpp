6dd14c87cc6fa14c1aeea17d640e28a8d91df72f
Jaoyad Niloy <137300704+Jaoyad@users.noreply.github.com>
2023-06-21 20:49:13 +0600

    Update E-ticketing.cpp

--- E-ticketing.cpp
+++ E-ticketing.cpp
@@ -3,10 +3,7 @@ Ticket Booking
 Ticket Cancel
 Ticket Status
 Add Match(for officials)
-exit
 About us*/
-
-
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -17,18 +14,13 @@ About us*/
 int main()
 {
 
-printf("\t\t\t\t==Wellcome to Ticket Booking System==\n");
-  printf("\t\t\t [1]=Add Match(For Officials\n");
-    printf("\t\t\t[2]=View MAtch Schedule\n");
-  priintf("\t\t\t[3]=Ticket Booking\n");
+printf("\t\t==Wellcome to Ticket Booking System==\n");
+  printf("\t\t\t[1]=Add Match(For Officials)\n");
+    printf("\t\t\t[2]=View Match Schedule\n");
+  printf("\t\t\t[3]=Ticket Booking\n");
   printf("\t\t\t[4]=Ticket Cancel\n");
   printf("\t\t\t[5]=Ticket Status\n");
-  printf("\t\t\t[6]=Exit\n");
+  printf("\t\t\t[6]=About us\n");
+printf("\t\t\t[7]=Exit\n");
 
-
-
-
-
-
-  
 }
